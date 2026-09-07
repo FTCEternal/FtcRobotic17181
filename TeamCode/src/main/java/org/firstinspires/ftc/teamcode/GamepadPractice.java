b6cf75e965d0e46bdea87e951abdd3350b673867
Timothy Zhou <timothy.ty.zhou@gmail.com>
2026-09-07 16:35:54 -0400

    i added three demo javas

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadPractice.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@TeleOp
+public class GamepadPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    //hold start button + a to recognize the gamepad as gamepad1
+    //hold start button + b to recognize the gamepad as gamepad2
+    @Override
+    public void loop() {
+        // run 2 times of the second
+        double speedForward = gamepad1.left_stick_y / 2.0;  //make it half power
+        double diffXJoysticks = gamepad1.left_stick_x-gamepad1.right_stick_x;
+        double sumTriggers = gamepad1.left_trigger+gamepad1.right_trigger;
+
+        telemetry.addData("left stick x",gamepad1.left_stick_x);
+        telemetry.addData("left stick y",gamepad1.left_stick_y);
+        telemetry.addData("a button",gamepad1.a);
+
+        telemetry.addData("speed forward",speedForward);
+        telemetry.addData("difference x",diffXJoysticks);
+        telemetry.addData("sum triggers",sumTriggers);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablePractice.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+//Class Name, capital first word
+public class VariablePractice extends OpMode {
+    /*data types example
+    int robotHeight;
+    double motoSpeed;
+    boolean clawClosed;
+    robotHeight = 10;
+    motoSpeed = 0.5;
+    clawClosed =true;
+     */
+    @Override
+    public void init() {
+        //Variable name, lower first Word, upper Second word
+        int teamNumber =17181;
+        string teamName = "xxxxx"
+        double motoSpeed=0.75;
+        boolean clawClosed=true;
+        string coachName="Nihar";
+
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Team Name", teamName);
+        telemetry.addData("motor speed", motoSpeed);
+        telemetry.addData("claw closed", clawClosed);
+        telemetry.addData("coach",coachName);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
